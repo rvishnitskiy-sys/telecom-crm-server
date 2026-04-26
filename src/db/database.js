3101009428cb6c7f6f399f2df3c3aeb6cc12570f
RomanV <r.vishnitskiy@gmail.com>
2026-04-26 17:54:35 +0300

    Remove .env from repository

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
-    process.env.NODE_ENV === "production"
+    process.env.DATABASE_URL && process.env.DATABASE_URL.includes("railway")
       ? { rejectUnauthorized: false }
       : false,
 });
